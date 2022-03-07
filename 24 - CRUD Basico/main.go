10375e42b3e419b8efde6de10d5df54533fb8ccb
Caio <caiordgs@outlook.com>
2022-03-07 15:53:34 -0300

    PUT users.

--- 24 - CRUD Basico/main.go
+++ 24 - CRUD Basico/main.go
@@ -15,6 +15,7 @@ func main() {
 	router.HandleFunc("/usuarios", servidor.CriarUsuario).Methods(http.MethodPost)
 	router.HandleFunc("/usuarios", servidor.BuscarUsuarios).Methods(http.MethodGet)
 	router.HandleFunc("/usuarios/{id}", servidor.BuscarUsuario).Methods(http.MethodGet)
+	router.HandleFunc("/usuarios/{id}", servidor.AtualizarUsuario).Methods(http.MethodPut)
 
 	fmt.Println("Conectado na porta 5001.")
 	log.Fatal(http.ListenAndServe(":5001", router))
