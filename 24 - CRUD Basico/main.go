ed0d1595c582b678c2baf52373f1a708ff84488a
Caio <caiordgs@outlook.com>
2022-03-07 16:10:20 -0300

    DELETE Users.

--- 24 - CRUD Basico/main.go
+++ 24 - CRUD Basico/main.go
@@ -16,7 +16,8 @@ func main() {
 	router.HandleFunc("/usuarios", servidor.BuscarUsuarios).Methods(http.MethodGet)
 	router.HandleFunc("/usuarios/{id}", servidor.BuscarUsuario).Methods(http.MethodGet)
 	router.HandleFunc("/usuarios/{id}", servidor.AtualizarUsuario).Methods(http.MethodPut)
+	router.HandleFunc("/usuarios/{id}", servidor.DeletarUsuario).Methods(http.MethodDelete)
 
-	fmt.Println("Conectado na porta 5001.")
-	log.Fatal(http.ListenAndServe(":5001", router))
+	fmt.Println("Conectado na porta 5000.")
+	log.Fatal(http.ListenAndServe(":5000", router))
 }
